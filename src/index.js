adfc34e0315281c757dc59d2a3f1e589e91f9bba
natew <natewienert@gmail.com>
2017-02-17 11:37:50 -0800

    :fire: simplify helpers

--- src/index.js
+++ src/index.js
@@ -7,37 +7,9 @@ import ref from './ref'
 export { CompositeDisposable } from 'sb-event-kit'
 export { setTimeout, setInterval, addEvent } from './events'
 
-const helpers = {
+export default {
   addEvent,
   setTimeout,
   setInterval,
   ref,
-}
-
-export default helpers
-
-export interface HelpfulClass {
-  addEvent: Function;
-  setTimeout: Function;
-  setInterval: Function;
-  ref: (() => Cursor);
-}
-
-// extend a class with given names
-export function inherit(klass: Object, ...names: Array<string>): void {
-  if (!names.length) {
-    // DEFAULT helpers to inherit
-    names = ['addEvent', 'setTimeout', 'setInterval', 'ref']
-  }
-  const methods = Object.keys(helpers)
-    .filter(k => names.indexOf(k) > -1)
-    .reduce((o, key) => ({ ...o, [key]: helpers[key] }), {})
-  Object.assign(klass.prototype, methods)
-}
-
-// mixin
-export const addHelpers = (which) => Klass => {
-  const helpers = which || ['addEvent', 'setTimeout', 'setInterval', 'ref']
-  inherit(Klass, ...helpers)
-  return Klass
 }
