8f16f2ee11b750fed7d47daab7dea4099490cf81
natew <natewienert@gmail.com>
2017-02-24 17:21:13 -0800

    :bug: fix isClass

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ export { CompositeDisposable } from 'sb-event-kit'
 export { setTimeout, setInterval, addEvent } from './events'
 export ref from './ref'
 
-export function isClass(class: Class) {
+export function isClass(Thing: Class) {
   function fnBody(fn) {
     return toString.call(fn).replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'');
   }
-  return (typeof fn === 'function' && (
-    /^class\s/.test(toString.call(fn)) ||
-    /^.*classCallCheck\(/.test(fnBody(fn))
+  return typeof fn === 'function' && (
+    /^class\s/.test(toString.call(Thing)) ||
+    /^.*classCallCheck\(/.test(fnBody(Thing))
   )
 }
