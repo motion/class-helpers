decd76d01f83c7a35011a974197de5c00234de50
natew <natewienert@gmail.com>
2017-02-24 16:36:33 -0800

    :new: isClass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 export { CompositeDisposable } from 'sb-event-kit'
 export { setTimeout, setInterval, addEvent } from './events'
 export ref from './ref'
+
+export function isClass(class: Class) {
+  function fnBody(fn) {
+    return toString.call(fn).replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'');
+  }
+  return (typeof fn === 'function' && (
+    /^class\s/.test(toString.call(fn)) ||
+    /^.*classCallCheck\(/.test(fnBody(fn))
+  )
+}
